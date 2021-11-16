3f80c249710a18a7748306bbd596d2016db1c69b
sebongile-stack <88289279+sebongile-stack@users.noreply.github.com>
2021-11-16 09:04:41 +0200

    Modified the application.

---
+++ PhotoAlbumProject2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoAlbumProject2
+{
+    public class Class1
+    {
+        public static int Count(int num1)
+        {
+            int result = 1;
+            return result;
+        }
+    }
+}
